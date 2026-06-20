bab776e6f5d48d48fb96c0a7e05b75ff8006c267
dennis <mvdev@tutamail.com>
2026-06-20 18:52:10 -0500

    Cambio en id para mejor legibilidad

--- backend/common/models.py
+++ backend/common/models.py
@@ -2,13 +2,18 @@ import uuid
 from django.db import models
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    id = modelis.UUIDField(
+            primary_key = True,
+            default = uuid.uuid4,
+            editable = False
+        )
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    
+
     # Crea la clase abstracta BaseModel
     # No se crea una tabla BaseModel, pero si se crean
     # los campos en cada clase que hereda de BaseModel
     class Meta:
         abstract = True
 
+    
